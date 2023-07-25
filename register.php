9e812680feaa450c67815f25a43fa3b3790cbd84
Kidus <98009847+kid-us@users.noreply.github.com>
2023-07-25 20:27:01 +0300

    Update register.php

--- register.php
+++ register.php
@@ -3,55 +3,55 @@
 // session_start();
 $error = "";
 $passError = "";
-include ("Php/conn.php");
+include("Php/conn.php");
 
 
-if (isset($_POST['register'])){
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $userEmail = mysqli_real_escape_string($conn, $_POST['reg-email']);
-    $password =  mysqli_real_escape_string($conn,$_POST['reg-password']);
-    $confirmPass = mysqli_real_escape_string($conn, $_POST['con-password']);
-    // Hash password
-    $hashPassword = md5($password);
-    $hasConfirmPassword = md5($confirmPass);
+if (isset($_POST['register'])) {
+  $username = mysqli_real_escape_string($conn, $_POST['username']);
+  $userEmail = mysqli_real_escape_string($conn, $_POST['reg-email']);
+  $password = mysqli_real_escape_string($conn, $_POST['reg-password']);
+  $confirmPass = mysqli_real_escape_string($conn, $_POST['con-password']);
+  // Hash password
+  $hashPassword = md5($password);
+  $hasConfirmPassword = md5($confirmPass);
 
-    if (!empty($password) && !empty($confirmPass) && !empty($username) && !empty($userEmail)){
-        if(strlen($password) >= 6){
-            if($hashPassword === $hasConfirmPassword){
-              // username existence checker  
-              $sql = "SELECT username FROM customer WHERE username = '$username'";
-              $result = mysqli_query($conn, $sql);
-              $count = mysqli_num_rows($result);
+  if (!empty($password) && !empty($confirmPass) && !empty($username) && !empty($userEmail)) {
+    if (strlen($password) >= 6) {
+      if ($hashPassword === $hasConfirmPassword) {
+        // username existence checker  
+        $sql = "SELECT username FROM customer WHERE username = '$username'";
+        $result = mysqli_query($conn, $sql);
+        $count = mysqli_num_rows($result);
 
-              if($count > 0){ //if there is no username like the input
-                $error = "Username already exist";
-              }else{
-                
-              // username existence checker  
-                $mysql = "SELECT email FROM customer WHERE email = '$userEmail'";
-                $res = mysqli_query($conn, $mysql);
-                $rows = mysqli_num_rows($res);
+        if ($count > 0) { //if there is no username like the input
+          $error = "Username already exist";
+        } else {
 
-                if($rows > 0){ //if there is no email like the input
-                  $error = "Email already Exist";
-                }else{
-                  $sql = "INSERT INTO customer (username, email ,password) VALUES ('$username', '$userEmail', '$hashPassword')";
-                  $result = mysqli_query($conn, $sql);
-                  if($result){
-                      header("Location: login.php");
-                  }
-                }
-              }
-              
-            }else{
-              $passError = "Password doesn't match";
+          // username existence checker  
+          $mysql = "SELECT email FROM customer WHERE email = '$userEmail'";
+          $res = mysqli_query($conn, $mysql);
+          $rows = mysqli_num_rows($res);
+
+          if ($rows > 0) { //if there is no email like the input
+            $error = "Email already Exist";
+          } else {
+            $sql = "INSERT INTO customer (username, email ,password) VALUES ('$username', '$userEmail', '$hashPassword')";
+            $result = mysqli_query($conn, $sql);
+            if ($result) {
+              header("Location: login.php");
             }
-        }else{
-          $passError = "Password needs to be more than 6 characters";
+          }
         }
-    }else{
-      $error = "Please fill the form!";
+
+      } else {
+        $passError = "Password doesn't match";
+      }
+    } else {
+      $passError = "Password needs to be more than 6 characters";
     }
+  } else {
+    $error = "Please fill the form!";
+  }
 
 }
 
@@ -60,89 +60,75 @@ if (isset($_POST['register'])){
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="/Img/reg-ico.ico" type="image/x-icon">
-    <link rel="stylesheet" href="Bootstrap/css/bootstrap.css" />
-    <link rel="stylesheet" href="Css/modal.css" />
-    <link rel="stylesheet" href="Css/style.css" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Grape+Nuts&display=swap"
-      rel="stylesheet"
-    />
 
-    <title>Register</title>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="/Img/reg-ico.ico" type="image/x-icon">
+  <!-- <link rel="stylesheet" href="Bootstrap/css/bootstrap.css" /> -->
+  <!-- Bootstrap CDN -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+  <!-- Bootstrap Icon-->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="Css/modal.css" />
+  <link rel="stylesheet" href="Css/style.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Grape+Nuts&display=swap" rel="stylesheet" />
+
+  <title>Register</title>
 </head>
 <body">
-    <div class="container">
+  <div class="container">
+    <div class="row justify-content-center mt-5 p-5 shadow-lg rounded bg-light small fw-semibold">
+      <div class="col-lg-6 col-md d-none d-md-block">
+        <img src="Img/undraw_fans_re_cri3.svg" class="img-fluid" alt="">
+      </div>
 
-    <div class="d-flex justify-content-center mt-5">
+      <div class="col-lg-6 col-md-6 col-12">
+        <form action="register.php" method="POST" class="px-lg-5">
+          <p class="fs-2 my-5">Sign In</p>
 
-      <div id="register-style" class="shadow-lg rounded">
-      <form action="register.php" method="POST">
-          <div class="text-center">
-            <img src="Img/register.png" alt="" width="50px" />
-            <br />
-            <a href="login.php" class="btn btn-outline-secondary mt-4 w-25 font-weight-bold" >Login</a>
-            <a type="button" class="btn btns mt-4 w-25 bg-info pt-2 text-light" id="register">
-              Register
-            </a>
-          </div>
-          <br />
-  
-          <p class="small font-weight-bold text-center text-danger"><?php echo $error;?></p>
-  
+          <p class="small font-weight-bold text-center text-danger">
+            <?php echo $error; ?>
+          </p>
+
           <div class="form-group">
-            <input
-              type="text"
-              name="username"
-              class="form-control-plaintext ml-5 w-75"
-              placeholder="Username"
-            />
-            <input
-              type="email"
-              name="reg-email"
-              class="form-control-plaintext ml-5 w-75"
-              placeholder="Email"
-              />
-              
-              <p id="error" style="padding: 0; margin: 0;" class="small pt-2 text-danger font-weight-bold mr-5 ml-5 p-0 text-justify"> <?php echo $passError;?></p>
-  
-              <input
-              type="password"
-              name="reg-password"
-              id="password"
-              class="form-control-plaintext w-75 ml-5"
-              placeholder="Password"
-              />
-  
-              <p style="padding: 0; margin: 0;" id="password-info" class="small mr-5 ml-5 p-0 text-justify text-primary font-weight-bold"> Your Password needs to be at least 6 characters please make it secure</p>
-  
-              <input
-              type="password"
-              name="con-password"
-              class="form-control-plaintext w-75 ml-5"
-              placeholder="Confirm Password"
-              />
-  
-            <button
-              type="submit"
-              name="register"
-              class="btn btn-outline-danger float-right mr-5 mt-4 w-25"
-            >
+            <label for="username">Username</label>
+            <input type="text" name="username" class="form-control-plaintext mb-3 w-75" />
+            <label for="reg-email">Email</label>
+            <input type="email" name="reg-email" class="form-control-plaintext mb-3 w-75" />
+
+            <p id="error" style="padding: 0; margin: 0;"
+              class="small pt-2 text-danger font-weight-bold p-0 text-justify">
+              <?php echo $passError; ?>
+            </p>
+            <label for="reg-password">Password</label>
+            <input type="password" name="reg-password" id="password" class="form-control-plaintext mb-3 w-75" />
+
+            <p style="padding: 0; margin: 0;" id="password-info" class="small p-0 text-justify text-info mb-3"> Your
+              Password needs to be at least
+              6 characters please make it secure</p>
+
+            <label for="con-password">Confirm Password</label>
+            <input type="password" name="con-password" class="form-control-plaintext mb-3 w-75" />
+
+            <button type="submit" name="register" class="btn btn-success mt-4 w-50">
               Register
             </button>
           </div>
+          <p class="mt-4">
+            <a href="login.php" class="text-decoration text-primary fw-semibold mt-5 w-25">Already have an account?</a>
+          </p>
         </form>
       </div>
     </div>
+
     <div class="d-flex justify-content-center m-5">
       <a href="index.php">
-        <img src="/Img/logo.png" alt="Our shop logo" width="150px">
+        <img src="Img/logo.png" alt="Our shop logo" width="150px">
       </a>
     </div>
-    </div>
-</body>
+  </div>
+  </body>
+
 </html>
