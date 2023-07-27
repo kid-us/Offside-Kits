87f6ef2189ad65414c8c0490d64d97b149c4a013
Kidus <98009847+kid-us@users.noreply.github.com>
2023-07-27 23:31:34 +0300

    Update adminlog.php

--- adminlog.php
+++ adminlog.php
@@ -1,103 +1,83 @@
-
 <?php
 
 session_start();
 unset($_SESSION['name']);
-include ("Php/conn.php");
+include("Php/conn.php");
 
 $msg = '';
 
-if (isset($_POST['login'])){
+if (isset($_POST['login'])) {
   $_SESSION['last_login_time'] = time();
 
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $pass = md5(mysqli_real_escape_string($conn,$_POST['password']));
+  $email = mysqli_real_escape_string($conn, $_POST['email']);
+  $pass = md5(mysqli_real_escape_string($conn, $_POST['password']));
 
-    $sql = "SELECT * FROM admin WHERE email = '$email' AND password = '$pass'";
+  $sql = "SELECT * FROM admin WHERE email = '$email' AND password = '$pass'";
 
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-    //  while($row){
-        $count = mysqli_num_rows($result);
-        if($count == 1){
-          $msg  = 'Logged in';
-        //  echo "User Name: {$row['username']}";
-          $adminName =  "{$row['username']}";
-          $_SESSION['name'] = $adminName;
-          header("Location: admin.php?Successfully Logged In ");
-        }else{
-          $msg =  "Incorrect Email or Password";
-        }
-    // }
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+  //  while($row){
+  $count = mysqli_num_rows($result);
+  if ($count == 1) {
+    $msg = 'Logged in';
+    //  echo "User Name: {$row['username']}";
+    $adminName = "{$row['username']}";
+    $_SESSION['name'] = $adminName;
+    header("Location: admin.php?Successfully Logged In ");
+  } else {
+    $msg = "Incorrect Email or Password";
+  }
+  // }
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="/Img/admin-ico.ico" type="image/x-icon">
-    <link rel="stylesheet" href="Bootstrap/css/bootstrap.css" />
-    <link rel="stylesheet" href="Css/admin.css" />
-    <title>Admin</title>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Grape+Nuts&display=swap"
-      rel="stylesheet"
-    />
-  </head>
-  <body>
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-lg-6 col-md-8 col-sm-12 mt-5"></div>
 
-        <form action="adminlog.php" method="POST" class="shadow-lg">
-          <div class="text-center">
-            <img src="Img/programmer.png" alt="" width="60px" />
-            <br />
-            <p class="btn btn-info mt-4 w-50">
-              Admin Login
-            </p>
-          </div>
-          <br />
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="shortcut icon" href="/Img/admin-ico.ico" type="image/x-icon">
+  <!-- Bootstrap CDN -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+  <!-- Bootstrap Icon-->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="Css/admin.css" />
+  <title>Admin Login</title>
+  <link href="https://fonts.googleapis.com/css2?family=Grape+Nuts&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+  <div class="container mt-5 p-5">
+    <div class="row justify-content-center bg shadow-lg p-5 rounded fw-semibold small">
+      <p class="fs-5">Login Our Admin</p>
+      <div class="col-6 mt-5">
+        <form action="adminlog.php" method="POST">
 
           <p class="text-center text-danger">
-            <?Php echo $msg ;?>
+            <?Php echo $msg; ?>
           </p>
 
-          <input
-            type="email"
-            name="email"
-            class="form-control-plaintext ml-5 w-75"
-            placeholder="Email"
-          />
-          <input
-            type="password"
-            name="password"
-            id="pass"
-            class="form-control-plaintext ml-5 w-75"
-            placeholder="Password"
-          />
-         
-
-          <input
-            type="checkbox"
-            id="show-password"
-            class="form-check-inline mt-3 ml-5"
-          />
-          <span class="small">Show Password</span>  <br />
-          <button
-            type="submit"
-            name="login"
-            class="btn btn-outline-success ml-5 mt-3 w-25"
-          >
+          <label for="email">Email</label>
+          <input type="email" name="email" class="form-control-plaintext w-75 my-3" />
+          <label for="email">Password</label>
+          <input type="password" name="password" id="pass" class="form-control-plaintext w-75 my-3" />
+          <input type="checkbox" id="show-password" class="form-check-inline mt-3 ml-5" />
+          <span class="small">Show Password</span> <br />
+          <button type="submit" name="login" class="btn btn-success mt-5 w-25">
             Login
           </button>
         </form>
       </div>
+
+      <div class="col-6 mt-5">
+        <img src="Img/admin.svg" alt="" class="img-fluid">
+      </div>
     </div>
-    <!-- <script src="/accountPage.js"></script> -->
-    <script src="JS/password.js"></script>
-  </body>
+  </div>
+  <!-- <script src="/accountPage.js"></script> -->
+  <script src="JS/password.js"></script>
+</body>
+
 </html>
