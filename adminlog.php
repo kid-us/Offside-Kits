311b120fe8792700e8d02680a2adb72bdb60836d
Kidus <98009847+kid-us@users.noreply.github.com>
2023-08-02 09:29:20 +0300

    Update adminlog.php

--- adminlog.php
+++ adminlog.php
@@ -38,14 +38,13 @@ if (isset($_POST['login'])) {
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="shortcut icon" href="/Img/admin-ico.ico" type="image/x-icon">
   <!-- Bootstrap CDN -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
   <!-- Bootstrap Icon-->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
   <link rel="stylesheet" href="Css/admin.css" />
+  <link rel="website icon" href="Img/web-logo.png">
   <title>Admin Login</title>
-  <link href="https://fonts.googleapis.com/css2?family=Grape+Nuts&display=swap" rel="stylesheet" />
 </head>
 
 <body>
