80a3836d3914731d95b41baedd8a6e62ee733fe4
Kidus <98009847+kid-us@users.noreply.github.com>
2023-08-02 09:28:44 +0300

    Update about.php

--- about.php
+++ about.php
@@ -11,7 +11,6 @@ session_start();
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="/Img/cart-ico.ico" type="image/x-icon">
     <!-- Bootstrap CDN -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <!-- Bootstrap Icon-->
@@ -22,8 +21,9 @@ session_start();
     <link rel="stylesheet" href="Css/modal.css" />
     <!-- Animated scrollbar link -->
     <link rel="stylesheet" href="Css/aos.css">
+    <link rel="website icon" href="Img/web-logo.png">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <title>About</title>
 </head>
 
